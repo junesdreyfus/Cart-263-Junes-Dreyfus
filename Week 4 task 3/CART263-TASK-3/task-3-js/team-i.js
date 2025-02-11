22d8855fa8744fb9c37360f58e05d1e01f7e0c9c
gamerjeannedielman <junes.dreyfus@gmail.com>
2025-02-11 10:26:17 -0500

    Task 3 week 4
    *Completed task 3
    *Added animC
    *pressing space bar randomizes background color, increases 'dosage' variable, increases the font size, and pull from an array with a randomized Index

---
+++ Week 4 task 3/CART263-TASK-3/task-3-js/team-i.js
@@ -0,0 +1,192 @@
+setup_I();
+/** THEME: JOY  */
+function setup_I() {
+  console.log("in i");
+  /**************************************************** */
+  //get the buttons
+  activateButtons_I(`#TEAM_I`, "ani_canvI");
+
+  /**************************************************** */
+  /* NO NEED TO MODIFY THIS FUNCTION :) */
+  /*** helper function to activate buttons */
+  /**************************************************** */
+  function activateButtons_I(team, teamCanvas) {
+    let teamButtons = document.querySelectorAll(`${team} .team-nav p`);
+    //2:
+    console.log(teamButtons);
+    for (let button of teamButtons) {
+      button.addEventListener("click", buttonCallBack);
+
+      function buttonCallBack(e) {
+        switch (this.textContent) {
+          case "1": {
+            console.log("A");
+            //reset the canvases
+            resetCanvases(`${team} .aniCanvas`);
+            //reset buttons
+            resetButtons(teamButtons, this);
+            //activate canvas A
+            document.getElementById(`${teamCanvas}_A`).style.display = "block";
+            //run first
+            aniA(document.getElementById(`${teamCanvas}_A`));
+
+            break;
+          }
+          case "2": {
+            console.log("B");
+            resetCanvases(`${team} .aniCanvas`);
+            //reset buttons
+            resetButtons(teamButtons, this);
+            //activate canvas B
+            document.getElementById(`${teamCanvas}_B`).style.display = "block";
+            //run second
+            aniB(document.getElementById(`${teamCanvas}_B`));
+            break;
+          }
+          case "3": {
+            console.log("C");
+            //reset the canvases
+            resetCanvases(`${team} .aniCanvas`);
+            //reset buttons
+            resetButtons(teamButtons, this);
+            //activate canvas C
+            document.getElementById(`${teamCanvas}_C`).style.display = "block";
+            //run third
+            aniC(document.getElementById(`${teamCanvas}_C`));
+            break;
+          }
+          case "4": {
+            console.log("D");
+            break;
+          }
+          case "5": {
+            console.log("E");
+            break;
+          }
+          case "6": {
+            console.log("F");
+            break;
+          }
+        }
+      }
+    } //for
+  }
+   /**************** ANI A ************************************ */
+  /** PUT ALL YOUR CODE FOR ANIMATION A INSIDE  HERE */
+  /**************** ANI A ************************************ */
+  /**************** TASK *******************************************
+   * YOU CAN USE ALL NOTES --- and see my examples in team-h.js for inspiration and possibly help:)
+   * 1: use the function window.requestAnimationFrame() to create an animation
+   * i.e. a reoccuring pattern - you can use simple shapes and colors, images etc...
+   * 2: create a way to start and stop the animation using a
+   * custom made button and add a mouse click event listener to either start/stop the animation
+   *
+   * NOTE::: PLEASE::: if you add any custom css PLEASE use the style.css and prefix any class names with your team label
+   * i.e. you want to create a custom div class and you are in "Team_A" then call your class TEAM_A_ANI_A_Div -
+   * this is so that your styles are not overriden by other teams.
+   * NOTE::: All your code is to be added here inside this function -
+   * remember you can define other functions inside....
+   * Do not change any code above or the HTML markup.
+   * **/
+
+  function aniA(parentCanvas) {
+    console.log("in A");
+  }
+  /**************** ANI B ************************************ */
+  /** PUT ALL YOUR CODE FOR ANIMATION B INSIDE  HERE */
+  /**************** ANI B ************************************ */
+  /**************** TASK *******************************************
+   * YOU CAN USE ALL NOTES --- and see my examples in team-h.js for inspiration and possibly help:)
+   * 1: use the function window.setInterval() to create a pattern that changes over time
+   * i.e. fading out/ in, growing bigger/smaller, appear/disappear, add, remove...
+   *  - you can use simple shapes and colors, images etc...
+   * 2: add in a / some mouse click event listener(s) somewhere to make the sketch interactive
+
+
+   *
+   * NOTE::: PLEASE::: if you add any custom css PLEASE use the style.css and prefix any class names with your team label
+   * i.e. you want to create a custom div class and you are in "Team_A" then call your class TEAM_A_ANI_A_Div -
+   * this is so that your styles are not overriden by other teams.
+   * NOTE::: All your code is to be added here inside this function -
+   * remember you can define other functions inside....
+   * Do not change any code above or the HTML markup.
+   * **/
+  function aniB(parentCanvas) {
+
+  }
+  /**************** ANI C ************************************ */
+  /** PUT ALL YOUR CODE FOR INTERACTIVE PATTERN C INSIDE  HERE */
+  /**************** ANI C ************************************ */
+ /**************** TASK *******************************************
+   * YOU CAN USE ALL NOTES --- and see my examples in team-h.js for inspiration and possibly help:)
+   * 1: use the PROVIDED keyup/down callbacks `windowKeyDownRef` and/or `windowKeyUpnRef` to handle keyboard events
+   * 2: create an interactive pattern/sketch based on keyboard input. Anything goes.
+   * do not use  requestAnimationFrame(), setInterval() nor setTimeout() -> meaning keep it simple ;)
+   * 
+   
+   * NOTE::: PLEASE::: if you add any custom css PLEASE use the style.css and prefix any class names with your team label
+   * i.e. you want to create a custom div class and you are in "Team_A" then call your class TEAM_A_ANI_A_Div -
+   * this is so that your styles are not overriden by other teams.
+   * NOTE::: All your code is to be added here inside this function -
+   * remember you can define other functions inside....
+   * Do not change any code above or the HTML markup.
+   * **/
+
+function aniC(parentCanvas) {
+  console.log("in C");
+  //increasingly maximalist vegetarian recipe generator/
+
+  let dosage = 1;
+  let fontSize = 10;
+
+  let recipeRandomizer = ["cup of milk", " cashew nut", "leek", "tbspn of ghee", "tbspn of olive oil", "apricot", "egg yolk", "egg white", "onion", "spring onion", "garlic clove", "tbspn of honey", "pinch of salt", "potato", "sweet potato", "sage leaf(for taste)", "tbspn of ginger powder (adds a kick)", "sliced mushroom", "tbspn of tomato paste"];
+
+  /*** THIS IS THE CALLBACK FOR KEY DOWN (DO NOT CHANGE THE NAME..) */
+  windowKeyDownRef = function (e) {
+
+    if (e.code === "Space") {
+      console.log("team-space down");
+
+//background color changes each time/
+      const randomColor = getRandomColor();
+      parentCanvas.style.backgroundColor = randomColor;
+      let randomIndex = Math.floor(Math.random() * recipeRandomizer.length);
+
+//so does the ingredient
+      let newWord = document.createElement("span");
+      newWord.textContent = "+"+dosage+recipeRandomizer[randomIndex];
+      newWord.classList.add("TEAM_I_word");
+      newWord.style.fontSize = fontSize + "px";
+      parentCanvas.appendChild(newWord);
+
+      //progressively larger quantities and font size/
+      dosage++;
+      fontSize += 3;
+
+    }
+  };
+
+    //function to generate a random background color/
+    function getRandomColor() {
+      const r = Math.floor(Math.random() * 256);
+      const g = Math.floor(Math.random() * 256);
+      const b = Math.floor(Math.random() * 256);
+  
+      return `rgb(${r}, ${g}, ${b})`;
+    }
+
+  /*** THIS IS THE CALLBACK FOR KEY UP (DO NOT CHANGE THE NAME..) */
+  windowKeyUpRef = function (e) {
+    if (e.code === "Space") {
+      console.log("space up");
+      console.log("team-space up");
+    }
+  };
+
+  // DO NOT REMOVE
+  window.addEventListener("keydown", windowKeyDownRef);
+  window.addEventListener("keyup", windowKeyUpRef);
+
+
+}
+}
