988ec48e84017860e19bde121cb5d822f63c9e37
gamerjeannedielman <junes.dreyfus@gmail.com>
2025-02-22 18:49:07 -0500

    Task 5 1 to 3
    *Finished the partC up to the last exercice.
    *I scrapped my progress on the latter as it's leading nowhere (atm)

---
+++ task 5/js/VideoObj.js
@@ -0,0 +1,86 @@
+class VideoObj {
+  constructor(x, y, w, h, videoElement, context) {
+    this.videoElement = videoElement;
+    this.context = context;
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+    this.shapeX = 10;
+    this.shapeY =10;
+    this.shapeCol = "#000000";
+ 
+
+    let filterButton_blur = document.getElementById("filter_button_blur");
+    let blurInput = document.getElementById("blurnum");
+    this.userProvidedBlur  = 0;
+    let self = this;
+
+    filterButton_blur.addEventListener("click", function () {
+      //get value from input field
+      self.userProvidedBlur = blurInput.value;
+      console.log(self.userProvidedBlur);
+    });
+
+    let filterButton_sepia = document.getElementById("filter_button_sepia");
+    let sepiaInput = document.getElementById("sepianum");
+    this.userProvidedSepia  = 0;
+
+    filterButton_sepia.addEventListener("click", function () {
+      //get value from input field divided by 100 to get a value between 0 and 1
+      self.userProvidedSepia = sepiaInput.value / 100;
+      console.log(self.userProvidedSepia);
+    });
+
+    let filterButton_hue = document.getElementById("filter_button_hue");
+    let hueInput = document.getElementById("huenum");
+    this.userProvidedHue  = 0;
+
+    filterButton_hue.addEventListener("click", function () {
+      self.userProvidedHue = hueInput.value;
+      console.log(self.userProvidedHue);
+    });
+
+    let filterButton_invert = document.getElementById("filter_button_invert");
+    let invertInput = document.getElementById("invertnum");
+    this.userProvidedInvert  = 0;
+
+    filterButton_invert.addEventListener("click", function () {
+      //get value from input field divided by 100 to get a value between 0 and 1
+      //This is the exact same logic as sepia/
+      self.userProvidedInvert = invertInput.value / 100;
+      console.log(self.userProvidedInvert);
+    });
+  }
+
+  
+  
+  display() {
+    this.context.save();
+    this.context.filter = `blur(${this.userProvidedBlur}px)`;
+//+= otherwise they fight for dominance for some reason./
+    this.context.filter += `sepia(${this.userProvidedSepia})`;
+
+    this.context.filter += `hue-rotate(${this.userProvidedHue}deg)`;
+    
+    this.context.filter += `invert(${this.userProvidedInvert})`;
+    
+    this.context.drawImage(this.videoElement, this.x, this.y, this.w, this.h);
+    this.context.fillStyle = this.shapeCol;
+    this.context.fillRect(this.shapeX, this.shapeY, 50,50)
+    this.context.restore();
+  }
+
+    //called when rectangle color is to be updated
+  changeColor(newCol){
+   /** FILL IN */
+  }
+  //called when rectangle Pos is to be updated
+  updatePositionRect(mx,my){
+     /** FILL IN */
+
+  }
+  update(videoElement) {
+    this.videoElement = videoElement;
+  }
+}
